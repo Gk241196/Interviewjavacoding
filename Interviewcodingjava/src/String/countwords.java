dff190487664e12d4aa776d7803f13f7a7ceddb0
GK241196 <Gk52707@gmai.com>
2025-02-05 19:18:57 +0530

    removed files

--- Interviewcodingjava/src/String/countwords.java
+++
@@ -1,9 +0,0 @@
-package String;
-public class countwords {
-	public static void main(String[] args) {
-        String str = "Ganesh Kumar Learning coding";
-        String[] ch=str.split(" ");
-        int length=ch.length;
-        System.out.println("Number of words are: " + length);
-	}
-}

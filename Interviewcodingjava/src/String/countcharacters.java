16d00fd597c6e24d47254d011e1643ca3122593c
GK241196 <Gk52707@gmai.com>
2025-02-07 13:26:46 +0530

    code changed

---
+++ Interviewcodingjava/src/String/countcharacters.java
@@ -0,0 +1,17 @@
+package String;
+leetcode.arrays
+leetcode.strings
+leetcode.dynamicprogramming
+public class countcharacters {
+	    public static void main(String[] args) {
+	    	String name="Ganesh Kumar";
+	    	char[] ch=name.toCharArray();//count the characters
+	    	int length1=ch.length;
+	    	System.out.println("Number of characters in string " +length1);
+	    	String[] ch1=name.split(" ");//count the words
+	    	int words=ch1.length;
+	    	System.out.println("Number of words in string " +words);
+	    }
+	}
+
+
--- src/String/checkanagrams.java
+++ src/String/checkanagrams.java
@@ -1,31 +1,19 @@
 package String;
-
+import java.util.Arrays;
 public class checkanagrams {
-    public static void main(String[] args) {
-        String str1 = "listen";
-        String str2 = "silent";
-        System.out.println("Are Anagrams: " + isAnagram(str1, str2));
-    }
-
-    public static boolean isAnagram(String str1, String str2) {
-        if (str1.length() != str2.length()) {
-            return false;
-        }
-
-        int[] charCount = new int[26]; // Assuming the strings contain only lowercase letters
-
-        for (int i = 0; i < str1.length(); i++) {
-            charCount[str1.charAt(i) - 'a']++;
-            charCount[str2.charAt(i) - 'a']--;
-        }
-
-        for (int count : charCount) {
-            if (count != 0) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-}
+	    public static void main(String[] args) {
+	        String str1 = "listen";
+	        String str2 = "silent";
+	        // Check if sorted versions of the strings are equal
+	        boolean isAnagram = isAnagram(str1, str2);
+	        System.out.println("Are Anagrams: " + isAnagram);
+	    }
+	    public static boolean isAnagram(String str1, String str2) {
+	        char[] arr1 = str1.toCharArray();
+	        char[] arr2 = str2.toCharArray();
+	        Arrays.sort(arr1);
+	        Arrays.sort(arr2);
+	        return Arrays.equals(arr1, arr2);
+	    }
+	}
 
